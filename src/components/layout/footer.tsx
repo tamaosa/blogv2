12da40aa0bd92bb585f3647c34ec38d80e6a8cec
Tamaosa <tamaki.osamu0101@gmail.com>
2021-04-02 23:35:18 +0900

    Update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,30 @@
 import React from "react"
+import { Link } from "gatsby"
 import { css } from "@emotion/react"
+
+import Bio from "../bio"
+
+const bioStyle = css`
+  display: flex;
+  justify-content: center;
+`
+
+const copyrightStyle = css`
+  text-align: right;
+`
 
 const Footer: React.FC = () => (
   <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.com">Gatsby</a>
+    <div css={bioStyle}>
+      <Bio />
+    </div>
+    <div css={copyrightStyle}>
+      <small>
+        <Link to="/">About</Link> | © 2021 Tamaosa, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </small>
+    </div>
   </footer>
 )
 

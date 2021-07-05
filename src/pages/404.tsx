5d2ffebd479d1b3b13a0568d65c7a59abf4cd558
tamaosa <tamaki.osamu0101@gmail.com>
2021-07-05 23:59:16 +0900

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,43 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import { css, keyframes } from "@emotion/react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const rotate = keyframes`
+  0% {
+    transform: translate3d(0,0,0)  rotate(0deg);
+  }
+
+  50% {
+    transform: translate3d(100vw,0,0) rotate(1080deg);
+  }
+`
 const NotFoundPage: React.FC = ({}) => {
   return (
     <Layout>
-      <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <SEO title="Not Found" />
+      <h1>😢Not Found</h1>
+      <p>お探しのページは移動されたか削除されたようです…</p>
+      <div>
+        <StaticImage
+          layout="fixed"
+          src="../../content/images/avator.png"
+          width={48}
+          height={48}
+          quality={95}
+          alt="プロフィール画像"
+          css={css`
+            position: fixed;
+            left: 0;
+            top: 50vh;
+            overflow: hidden;
+            animation: ${rotate} 7.5s ease infinite;
+            border-radius: 50%;
+          `}
+        />
+      </div>
     </Layout>
   )
 }

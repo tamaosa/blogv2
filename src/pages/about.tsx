899765beaf448bddf6eff86deb8a86108525005c
tamaosa <tamaki.osamu0101@gmail.com>
2021-08-31 00:36:30 +0900

    Update about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,12 +8,13 @@ import { SiteMetadata } from "../types/site-metadata"
 const AboutPage: React.FC = () => {
   const data = useStaticQuery<{
     site: {
-      siteMetadata: Pick<SiteMetadata, "social" | "repository">
+      siteMetadata: Pick<SiteMetadata, "siteUrl" | "social" | "repository">
     }
   }>(graphql`
     query AboutQuery {
       site {
         siteMetadata {
+          siteUrl
           social {
             twitter
             github
@@ -23,7 +24,7 @@ const AboutPage: React.FC = () => {
       }
     }
   `)
-  const { social, repository } = data.site.siteMetadata
+  const { siteUrl, social, repository } = data.site.siteMetadata
 
   return (
     <Layout>
@@ -32,6 +33,42 @@ const AboutPage: React.FC = () => {
       <h2>著者について</h2>
       <p>
         某メーカーの見習いソフトウェア開発者です。業務では開発製品のソフトウェアに関連することは全部やることになるらしい…
+      </p>
+      <h2>作ったものとか</h2>
+      <h3>
+        <a
+          href={siteUrl}
+          target="_blank"
+          rel="external noopener"
+          aria-label={`ブログ`}
+        >
+          このブログ
+        </a>
+      </h3>
+      <p>
+        このブログです。実は一度作り変えています。
+        <a
+          href={`https://github.com/gatsbyjs/gatsby-starter-blog`}
+          target="_blank"
+          rel="external noopener"
+          aria-label={`gatsby-starter-blog`}
+        >
+          gatsby-starter-blog
+        </a>
+        をベースに作成しました。（われながら）ダークモードの配色が気に入っています。
+      </p>
+      <h3>
+        <a
+          href={`https://tidey.netlify.app/`}
+          target="_blank"
+          rel="external noopener"
+          aria-label={`Tidey`}
+        >
+          Tidey
+        </a>
+      </h3>
+      <p>
+        （⚠️もう更新してない）タイドグラフを表示するだけのサイトです。ある程度は自分で作った？はじめてのサイトです。釣りが趣味なので作りました。
       </p>
       <h2>リンク</h2>
       <ul>

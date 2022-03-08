517bca0535c2c0218ac9588cfbadad26e452c0f4
tamaosa <55348837+tamaosa@users.noreply.github.com>
2022-03-09 02:34:55 +0900

    Add @emotion/jest

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,4 +1,9 @@
 const babelOptions = {
-  presets: ["babel-preset-gatsby", "@babel/preset-typescript"],
+  presets: [
+    "babel-preset-gatsby",
+    "@babel/preset-typescript",
+    "@emotion/babel-preset-css-prop",
+  ],
+  plugins: ["@emotion"],
 }
 module.exports = require("babel-jest").default.createTransformer(babelOptions)
